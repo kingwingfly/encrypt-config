81ad84c0c09440fcdc9f658d96b4645819b2e840
Louis <836250617@qq.com>
2024-07-24 21:35:19 +0800

    upgrade docs

--- encrypt-config/src/config.rs
+++ encrypt-config/src/config.rs
@@ -104,9 +104,9 @@ impl Cache {
 
 /// A struct that can be used to **cache** configuration values.
 /// This behaves like a native cache in CPU:
-/// 1. If cache hit, return the cached value when reading, while update the cached value and then write back when writing.
-/// 2. If cache miss, load the cached value from the source to cache when reading, while write and then load when writing.
-///
+/// 1. If cache hit, reading returns the cached value, while writing upgrades the cached value then set cache flag dirty.
+/// 2. If cache miss, reading loads the value from the source to cache, while writing saves the value to source then loads it to cache.
+/// 3. All caches values dirty will be written back when Config dropped.
 #[cfg_attr(
     feature = "secret",
     doc = "To avoid entering the password during testing, you can enable `mock` feature. This can always return the **same** Encrypter during **each** test."
@@ -131,7 +131,9 @@ impl Config {
     }
 
     /// Get an immutable ref from the config.
-    /// If the value is not found, it will be created with the default value.
+    /// If the value was not valid, it would try loading from source, and fell back to the default value.
+    ///
+    /// If the value was marked as writing, it would panic like `RefCell`.
     /// See [`ConfigRef`] for more details.
     pub fn get<T>(&self) -> ConfigRef<'_, T>
     where
@@ -141,7 +143,9 @@ impl Config {
     }
 
     /// Get a mutable ref from the config.
-    /// If the value is not found, it will be created with the default value.
+    /// If the value was not valid, it would try loading from source, and fell back to the default value.
+    ///
+    /// If the value was marked as writing, it would panic like `RefCell`.
     /// See [`ConfigMut`] for more details.
     pub fn get_mut<T>(&self) -> ConfigMut<'_, T>
     where
@@ -151,7 +155,8 @@ impl Config {
     }
 
     /// Take the ownership of the config value.
-    /// If the value is not found, it will be created with the default value.
+    ///
+    /// If the value was not valid, it would try creating from source, and fell back to the default value.
     /// This will remove the value from the config.
     pub fn take<T>(&self) -> T
     where
@@ -164,7 +169,7 @@ impl Config {
     ///
     /// T: (T1, T2, T3,)
     ///
-    /// If the value is not found, it will be created with the default value.
+    /// If the value was not valid, it would try loading from source, and fell back to the default value.
     /// See [`ConfigRef`] for more details.
     pub fn get_many<T>(&self) -> <T as Cacheable<((),)>>::Ref<'_>
     where
@@ -177,7 +182,7 @@ impl Config {
     ///
     /// T: (T1, T2, T3,)
     ///
-    /// If the value is not found, it will be created with the default value.
+    /// If the value was not valid, it would try loading from source, and fell back to the default value.
     /// See [`ConfigMut`] for more details.
     pub fn get_mut_many<T>(&self) -> <T as Cacheable<((),)>>::Mut<'_>
     where
@@ -190,7 +195,7 @@ impl Config {
     ///
     /// T: (T1, T2, T3,)
     ///
-    /// If the value is not found, it will be created with the default value.
+    /// If the value was not valid, it would try creating from source, and fell back to the default value.
     /// This will remove the value from the config.
     pub fn take_many<T>(&self) -> <T as Cacheable<((),)>>::Owned
     where
@@ -200,12 +205,11 @@ impl Config {
     }
 
     /// Save the config value manually.
-    /// Note that the changes you made through [`ConfigMut`]
-    /// will be saved as leaving the scope automatically.
     ///
     /// Ideally, it's better to change cache first and then set dirty flag when writing,
-    /// and save the value when the cache drops. However, this is hard to implement
-    /// for manual saving by now. So, one is supposed to use `get` and `get_mut` to change the value.
+    /// and save the value when the cache drops. However, this method is provided for manual control.
+    ///
+    /// Caution that this will not update the cache value.
     pub fn save<T>(&self, value: T) -> ConfigResult<()>
     where
         T: Source + Any + Send + Sync,
@@ -223,22 +227,10 @@ impl Config {
         }
         Ok(())
     }
-
-    // /// Flush all the dirty values to the source.
-    // pub fn flush(&self) {
-    //     for value in unsafe { &mut *self.cache.inner.get() }.values_mut() {
-    //         if value.flags.contains(CacheFlags::DIRTY) && value.flags.contains(CacheFlags::VALID) {
-    //             value.write_back();
-    //         }
-    //     }
-    // }
 }
 
-/// This holds a `RwLockReadGuard` of the config value until the end of the scope.
-/// It is used to get an immutable reference to the config value.
-/// One should drop it as soon as possible to avoid deadlocks.
-/// # Deadlocks
-/// - If you already held a [`ConfigMut`], [`Config::get()`] will block until you drop it.
+/// # Panic
+/// - If you already held a [`ConfigMut`], [`Config::get()`] will panic.
 pub struct ConfigRef<'a, T>
 where
     T: Source + Any,
@@ -247,13 +239,8 @@ where
     _marker: PhantomData<&'a T>,
 }
 
-/// This holds a `RwLockWriteGuard` of the config value until the end of the scope.
-/// It is used to get a mutable reference to the config value.
-/// One should drop it as soon as possible to avoid deadlocks.
-///
-/// The value will be written back as dropping if changed. If saving fails, it will print an error.
-/// # Deadlocks
-/// - If you already held a [`ConfigRef`] or [`ConfigMut`], [`Config::get_mut()`] will block until you drop it.
+/// # Panic
+/// - If you already held a [`ConfigRef`] or [`ConfigMut`], [`Config::get_mut()`] will panic.
 pub struct ConfigMut<'a, T>
 where
     T: Source + Any,
